223fa0fae252f864ee0f9a2a457daa4893c21cca
Bill Johnson <bjohnson@75f.io>
2024-10-07 10:07:54 -0500

    Initial check-in
    Signed-off-by: Bill Johnson <bjohnson@75f.io>

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    // The go to example for any programming language
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
